842d776b1168a173b90c0740eb7cc81f3cf7940a
virmon <virmontumulak@gmail.com>
2020-10-05 12:04:13 -0500

    Add fetch call to API endpoints

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -321,10 +321,16 @@ function defaultFetchOpts() {
 
 function getTracks() {
 	// GET request to `${SERVER}/api/tracks`
+	fetch(`${SERVER}/api/tracks`)
+		.then(res => res.json())
+		.catch(err => console.log("Problem with getTracks", err))
 }
 
 function getRacers() {
 	// GET request to `${SERVER}/api/cars`
+	fetch(`${SERVER}/api/cars`)
+		.then(res => res.json())
+		.then(err => console.log("Problem with getRacers", err))
 }
 
 function createRace(player_id, track_id) {
@@ -344,6 +350,9 @@ function createRace(player_id, track_id) {
 
 function getRace(id) {
 	// GET request to `${SERVER}/api/races/${id}`
+	fetch(`${SERVER}/api/races/${id}`)
+		.then(res => res.json())
+		.catch(err => console.log("Problem with getRace", err))
 }
 
 function startRace(id) {
@@ -359,4 +368,10 @@ function accelerate(id) {
 	// POST request to `${SERVER}/api/races/${id}/accelerate`
 	// options parameter provided as defaultFetchOpts
 	// no body or datatype needed for this request
+	fetch(`${SERVER}/api/races/${id}/accelerate`, {
+		method: 'POST',
+		...defaultFetchOpts()
+	})
+	.then(res => res.json())
+	.catch(err => console.log("Problem with accelerate", err))
 }
